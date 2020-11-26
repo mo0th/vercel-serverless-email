ab6495b72b844d6ddb3d4f95e2e8155219002f26
Soorria Saruva <soorria.ss@gmail.com>
2020-11-26 20:23:27 +1100

    Add some error handling

--- api/send-mail.ts
+++ api/send-mail.ts
@@ -1,13 +1,36 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 import sgMail from '@sendgrid/mail'
 
-sgMail.setApiKey(process.env.SENDGRID_KEY)
+const API_KEY = process.env.SENDGRID_KEY
+
+try {
+  sgMail.setApiKey(API_KEY)
+} catch (err) {
+  console.log(err)
+  process.exit(1)
+}
 
 const to = process.env.TO_EMAIL
 const from = process.env.FROM_EMAIL
 
 export default async (req: NowRequest, res: NowResponse) => {
   const { subject, text, html } = req.body
+
+  if (!to) {
+    console.error(
+      "Looks like you haven't set the `TO_EMAIL` environment variable"
+    )
+    res.status(500)
+    return
+  }
+
+  if (!from) {
+    console.error(
+      "Looks like you haven't set the `FROM_EMAIL` environment variable"
+    )
+    res.status(500)
+    return
+  }
 
   const msg = { to, from, subject, text, html }
 
